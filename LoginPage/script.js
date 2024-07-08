6f7b601a38587e9b1fc2fc0c6c155e59f8c22f82
truong <vuvantruongit2003@gmail.com>
2024-07-08 18:36:47 +0700

    Dựng giao diện LoginPage lần 2
    Hoàn thiện giao diện của mobile

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -1,3 +1,4 @@
+//Func xử lý việc người dùng xem mật khẩu
 document.getElementById("togglePassword").addEventListener("click", function () {
     var passwordField = document.getElementById("password");
     var icon = this.querySelector("i");
@@ -13,10 +14,19 @@ document.getElementById("togglePassword").addEventListener("click", function () 
     }  
 });
 
-//Ngăn hành động mặc định load trang của button và xử lý nghiệp vụ *****
+//Xử lý người dùng không nhập đủ tài khoản mật khẩu thì không cho phép Login
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const btnLoginCursor = document.getElementById('btn-login-cursor');
+function checkInputs() {
+  const inputsFilled = email.value.trim() !== '' && password.value.trim() !== '';
+  btnLoginCursor.classList.toggle('loginEnter', inputsFilled);
+}
+email.addEventListener('input', checkInputs);
+password.addEventListener('input', checkInputs);
+
+//Mặc định load trang khi bấm button và xử lý nghiệp vụ *****
 document.getElementById('btn-login-cursor').addEventListener('click', function(event) {
   event.preventDefault();
 
-  // Thực hiện các hành động khác ở đây nếu cần
-  console.log('Button clicked!');
 });
