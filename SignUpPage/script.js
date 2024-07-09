ad4c165c871c996b44fc6f98d070b123060f5112
truong <vuvantruongit2003@gmail.com>
2024-07-09 09:45:11 +0700

    Hoàn thiện giao diện SignUpPage

--- SignUpPage/script.js
+++ SignUpPage/script.js
@@ -1,32 +1,14 @@
-//Func xử lý việc người dùng xem mật khẩu
-document.getElementById("togglePassword").addEventListener("click", function () {
-    var passwordField = document.getElementById("password");
-    var icon = this.querySelector("i");
-
-    if (passwordField.type === "password") {
-      passwordField.type = "text";
-      icon.classList.remove("bi-eye");
-      icon.classList.add("bi-eye-slash");
-    } else {
-      passwordField.type = "password";
-      icon.classList.remove("bi-eye-slash");
-      icon.classList.add("bi-eye");
-    }  
-});
-
-//Xử lý người dùng không nhập đủ tài khoản mật khẩu thì không cho phép Login
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const btnLoginCursor = document.getElementById('btn-login-cursor');
+//Xử lý người dùng không nhập đủ số điện thoại thì không cho phép SignUp
+const phone = document.getElementById('phone');
+const btnSignUpCursor = document.getElementById('btn-signup-cursor');
 function checkInputs() {
-  const inputsFilled = email.value.trim() !== '' && password.value.trim() !== '';
-  btnLoginCursor.classList.toggle('loginEnter', inputsFilled);
+  const inputsFilled = phone.value.trim() !== '';
+  btnSignUpCursor.classList.toggle('signupEnter', inputsFilled);
 }
-email.addEventListener('input', checkInputs);
-password.addEventListener('input', checkInputs);
+phone.addEventListener('input', checkInputs);
 
 //Mặc định load trang khi bấm button và xử lý nghiệp vụ *****
-document.getElementById('btn-login-cursor').addEventListener('click', function(event) {
+document.getElementById('btn-signup-cursor').addEventListener('click', function(event) {
   event.preventDefault();
 
 });
